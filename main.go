0e8ee74a0e8938b2891ffc9634ea4df0db94cd1d
PlexCemex <plexcemex@proton.me>
2025-09-10 16:28:42 +1000

    First request hello and handler

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+	router := http.NewServeMux()
+	NewHelloHandler(router)
+	server := http.Server{
+		Addr: ":7080",
+		Handler: router,
+	}
+	fmt.Println("Server is listening on port 7080")
+	server.ListenAndServe()
 }
