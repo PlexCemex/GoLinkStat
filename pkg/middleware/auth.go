b800c4547f9de15e9871eb0eeef895b6c038a1b4
PlexCemex <plexcemex@proton.me>
2025-09-27 16:32:30 +1000

    make rew request with new context

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,21 +1,25 @@
 package middleware
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 	"projects/GoLinkStat/configs"
 	"projects/GoLinkStat/pkg/jwt"
 	"strings"
+)
+type key string
+
+const(
+	ContextEmailKey key = "ContextEmailKey"
 )
 
 func IsAuthed(next http.Handler, conf *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("authorization")
 		token := strings.TrimPrefix(authHeader, "Bearer ")
-		isValid, data := jwt.NewJWT(conf.Auth.Secret).Parse(token)
-		fmt.Println(token)
-		fmt.Println(isValid)
-		fmt.Println(data)
-		next.ServeHTTP(w, r)
+		_, data := jwt.NewJWT(conf.Auth.Secret).Parse(token)
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		newR := r.WithContext(ctx)
+		next.ServeHTTP(w, newR)
 	})
 }
