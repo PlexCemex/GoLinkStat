9db07224cd2acb6e2aef9905cb55579dfbced9b7
PlexCemex <plexcemex@proton.me>
2025-10-04 18:25:51 +1000

    TestLoginFail

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"projects/GoLinkStat/internal/auth"
@@ -19,10 +20,41 @@ func TestLoginSuccess(t *testing.T) {
 	})
 
 	resp, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
-	if err != nil{
+	if err != nil {
 		t.Fatal(err)
 	}
-	if resp.StatusCode != 200{
+	if resp.StatusCode != 200 {
 		t.Fatalf("Exepted %d, got %d", 200, resp.StatusCode)
 	}
+	token, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(token, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("Token empty")
+	}
+}
+
+func TestLoginFail(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a@a.ru",
+		Password: "2",
+	})
+
+	resp, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 401 {
+		t.Fatalf("Exepted %d, got %d", 401, resp.StatusCode)
+	}
+	// resp.Body.
 }
